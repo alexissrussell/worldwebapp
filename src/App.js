080e52b27b20a782dac2c8f35ae55faf91bd2728
alexissrussell <alexissrussell@gmail.com>
2018-02-13 11:59:48 -0800

    url

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ class App extends Component {
         
     }
     componentDidMount(){
-     this.socket = mySocket("https://worldwebapp.herokuapp.com/");
+     //this.socket = mySocket("https://worldwebapp.herokuapp.com/");
         
     }
     
@@ -31,7 +31,7 @@ class App extends Component {
             mode:1
             
         })
-        this.socket = mySocket("http://localhost:10001")
+        this.socket = mySocket("https://worldsockets.herokuapp.com")
         this.socket.emit("username", this.state.username);
         
         this.socket.on("usersjoined", (data)=>{
