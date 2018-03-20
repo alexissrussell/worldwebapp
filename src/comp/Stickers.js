ca9a1e3e37c132bcd211f66c958aeb63f0335ef6
Alexis Russell <alexissrussell@users.noreply.github.com>
2018-03-20 01:02:15 -0700

    Update Stickers.js

--- src/comp/Stickers.js
+++ src/comp/Stickers.js
@@ -21,7 +21,7 @@ class Stickers extends Component {
 	}
 	
 	componentDidMount(){
-		this.socket = mySocket("http://localhost:10000");
+		this.socket = mySocket("https://worldsockets.herokuapp.com/");
 		
 		this.socket.on("createimage", (data)=>{
 			this.setState({
